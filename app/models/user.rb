f316552e73ff4d29735aa53884352423dbdc9fa9
dbpatnodee <dbpatnode@hotmail.com>
2020-10-09 11:00:28 -0700

    changed user validation from username to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
     has_secure_password
     validates_presence_of :username, :email
-    validates_uniqueness_of :username, :username
+    validates_uniqueness_of :username, :email
 
 end
