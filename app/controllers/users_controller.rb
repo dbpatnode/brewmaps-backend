d92b5da92631f2900bcdf748c83fcfa802dba86f
dbpatnodee <dbpatnode@hotmail.com>
2020-10-09 15:35:13 -0700

    changed cors.rb origin to 3001

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
 
-        # def index
-        #     users = User.all
-        #     render json: users
-        # end
+        def index
+            users = User.all
+            render json: users
+        end
         
         # def create
         #     @user = User.create(user_params)
