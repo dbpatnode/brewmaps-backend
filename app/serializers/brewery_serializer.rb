06e81022db3696efb82d122526eff5ed3d24e06f
dbpatnodee <dbpatnode@hotmail.com>
2020-10-11 15:34:51 -0700

    changed serializers for breweries to include phone number and website

--- app/serializers/brewery_serializer.rb
+++ app/serializers/brewery_serializer.rb
@@ -1,5 +1,5 @@
 class BrewerySerializer < ActiveModel::Serializer
-  attributes :id, :brewery_name, :brewery_type, :street, :city, :state, :postal_code, :phone, :lng, :lat
+  attributes :id, :brewery_name, :brewery_type, :street, :city, :state, :postal_code, :lng, :lat, :phone, :website_url
   has_many :favorites
   has_many :notes
 end
